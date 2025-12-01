7ce15167a74bc82bde3ab7b0c619afa1f0e9f46e
Thien phuc <thienphuc484@gmail.com>
2025-12-01 13:41:59 +0700

    Convert array to vector

--- src/main.cpp
+++ src/main.cpp
@@ -11,13 +11,13 @@ int main() {
 
     const Shader myShader("asset/shader/shader.vs", "asset/shader/shader.fs");
 
-    constexpr float vertices[] = {
+    const std::vector vertices = {
         -0.5f, -0.5f, 0.0f,
         0.5f, -0.5f, 0.0f,
         0.0f,  0.5f, 0.0f
     };
 
-    constexpr unsigned int indices[] = {
+    const std::vector indices = {
         0, 1, 2
     };
 
@@ -29,10 +29,16 @@ int main() {
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER,
+        static_cast<GLsizeiptr>(vertices.size()) * static_cast<GLsizeiptr>(sizeof(float)),
+        vertices.data(),
+        GL_STATIC_DRAW);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+        static_cast<GLsizeiptr>(indices.size()) * static_cast<GLsizeiptr>(sizeof(unsigned int)),
+        indices.data(),
+        GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), static_cast<void *>(nullptr));
     glEnableVertexAttribArray(0);
