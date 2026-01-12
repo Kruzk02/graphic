625720166a49dcdc8054902c64bef07ce82762e6
Thien phuc <thienphuc484@gmail.com>
2026-01-12 10:58:12 +0700

    Change cube shader filename in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <application.h>
 
 int main() {
-    Application application {{900, 720, "asset/shader/shader.vs", "asset/shader/shader.fs"}};
+    Application application {{900, 720, "asset/shader/cube-shader.vs", "asset/shader/cube-shader.fs"}};
     application.run();
     return 0;
 }
