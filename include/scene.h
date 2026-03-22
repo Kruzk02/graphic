d91e0e9a9fb8c86ecf844f6c443d5bd7d07d4ac7
Thien phuc <thienphuc484@gmail.com>
2026-03-22 13:11:45 +0700

    Remove draw() in scene class

--- include/scene.h
+++ include/scene.h
@@ -1,14 +1,12 @@
 #pragma once
 #include "model.h"
+#include <vector>
 
 class Scene {
 public:
   void addModel(Model &&model) { models.push_back(std::move(model)); }
 
-  void draw(const Shader &shader) const {
-    for (const auto &m : models)
-      m.draw(shader);
-  }
+  std::vector<Model> &getModels() { return models; }
 
 private:
   std::vector<Model> models;
